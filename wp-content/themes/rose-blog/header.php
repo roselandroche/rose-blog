05e151a9ee46eceae61761fc4f4884df0aa3aabf
roselandroche <r.landroche@gmail.com>
2020-12-29 10:27:24 -0800

    Adding to header

--- wp-content/themes/rose-blog/header.php
+++ wp-content/themes/rose-blog/header.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 
-<html>
+<html <?php language_attributes(); ?> >
     <head>
+        <meta charset="<?php bloginfo('charset'); ?>" >
+        <meta name="viewport" content="width=device-width, initial-scale=1" >
         <?php wp_head(); ?>
     </head>
-    <body>
-        <h1>Rose's Blog</h1>
+    <body <?php body_class(); ?> >
+        <header>
+            <div>
+                <!-- logo could be added here -->
+                <h1><a href="<?php echo esc_url(site_url()); ?>">Rose's Blog</a></h1>
+                <!-- search icon could be added here -->
+            </div>
+        </header>
